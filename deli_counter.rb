0b31c7880092b314b6502e61de98fd812efa12e9
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-14 17:49:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,12 +23,13 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  katz_deli.pop
+  
   if katz_deli.length == 0 
      "There is nobody waiting to be served!"
   else
       
      "Currently serving #{katz_deli[0]}."
+     katz_deli.shift
       
   end
 end
