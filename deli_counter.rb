72dff4b16eaec49ddcb105b7d2cf1b5c11c82303
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-14 17:42:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,3 +21,12 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   name
 end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0 
+     "There is nobody waiting to be served!"
+  else
+     "Currently serving #{katz_deli.first}."
+      katz_deli.pop 
+  end
+end
