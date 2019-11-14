21b376ed24ca9d4bda66b48f053ba87b8bf1905f
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-14 17:17:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,15 @@
 katz_deli = []
 
 def line(array)
-  
+  if array.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Welcome"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line"
+  name
+end
